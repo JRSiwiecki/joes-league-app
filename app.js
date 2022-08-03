9752504425d8e5246f7568a97b5e9132e4c75af2
JRSiwiecki <JRSiwiecki@gmail.com>
2022-08-02 20:57:22 -0700

    Added image display based on champion

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
+require("dotenv").config();
+const api_key = process.env.API_KEY;
+
 const express = require("express");
 const ejs = require("ejs");
-require("dotenv").config();
+
+const { Kayn, REGIONS } = require('kayn')
+const kayn = Kayn(api_key)(/*{
+    region: REGIONS.NORTH_AMERICA,
+    apiURLPrefix: 'https://%s.api.riotgames.com',
+    locale: 'en_US',
+    debugOptions: {
+        isEnabled: true,
+        showKey: false,
+    },
+    requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: false,
+        shouldExitOn403: false,
+    },
+    cacheOptions: {
+        cache: null,
+        timeToLives: {
+            useDefault: false,
+            byGroup: {},
+            byMethod: {},
+        },
+    },
+}*/);
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 
-const api_key = process.env.API_KEY;
-
 app.get("/", (req, res) => {
-  res.render("home");
+
+  kayn.DDragon.Champion.list() // Implicitly targets 8.24.1
+    .callback(function(error, champions) {
+        let champName = champions.data.Aatrox.id;
+        console.log(champName);
+
+        res.render("home", {
+          championImage: champName,
+        });
+    });
+
+
 });
 
-
 app.use(express.static("public"));
+
 
 app.listen(port, () => {
   console.log("app has begun on port " + port);
