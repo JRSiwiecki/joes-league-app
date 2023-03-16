3fa2daedca1ec6de1b503ef0af088f9172286535
JRSiwiecki <JRSiwiecki@gmail.com>
2023-03-15 23:24:02 -0700

    Fix the app! Rejoice! Team builder works as intended, item generator needs work

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const riot_api_key = process.env.API_KEY;
 const express = require("express");
 const ejs = require("ejs");
 const jsonpath = require("jsonpath");
+const { Kayn, REGIONS } = require("kayn");
+
+const kayn =
+  Kayn(riot_api_key)(/*{
+    region: REGIONS.NORTH_AMERICA,
+    apiURLPrefix: 'https://%s.api.riotgames.com',
+    locale: 'en_US',
+    debugOptions: {
+        isEnabled: true,
+        showKey: false,
+    },
+    requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: false,
+        shouldExitOn403: false,
+    },
+    cacheOptions: {
+        cache: null,
+        timeToLives: {
+            useDefault: false,
+            byGroup: {},
+            byMethod: {},
+        },
+    },
+}*/);
 
 const app = express();
 const port = 3000;
@@ -93,8 +120,147 @@ app.use(express.static("public"));
 
 app.listen(port, () => {});
 
-function getRandomChampionsAndTitles() {}
+function getRandomChampionsAndTitles() {
+  // use kayn api to get list of champions as a .json
+  kayn.DDragon.Champion.list().callback((error, champions) => {
+    // turn champions.data json into array to get champion names
+    let championNameData = Object.keys(champions.data);
 
-function getRandomItems() {}
+    // choose 5 random champions and add them to randomChampList
+    for (let i = 0; i < 5; i++) {
+      let randomChamp;
+
+      // prevents duplicate champions from appearing
+      do {
+        randomChamp = championNameData[Math.floor(Math.random() * 161)];
+      } while (champImgList.indexOf(randomChamp) !== -1);
+
+      // retreives title for champion from the random champion
+      // couldnt figure out how to do this without jsonpath library
+      let champName = jsonpath.query(
+        champions,
+        "$.data." + randomChamp + ".name"
+      );
+      let champTitle = jsonpath.query(
+        champions,
+        "$.data." + randomChamp + ".title"
+      );
+
+      // push the random champ and their titles
+      champImgList.push(randomChamp);
+      champNameList.push(champName);
+      champTitleList.push(champTitle);
+    }
+  });
+}
+
+function getRandomItems() {
+  kayn.DDragon.Item.list().callback((error, items) => {
+    let itemData = Object.keys(items.data);
+
+    for (let i = 0; i < 6; i++) {
+      let randomItem;
+
+      do {
+        randomItem = itemData[Math.floor(Math.random() * 254)];
+      } while (itemList.indexOf(randomItem) !== -1);
+
+      // if it's in the store, it's allowed
+      // if it's consumable, it's not allowed
+      // if it's on summoners rift, it's allowed
+      // if it needs ornn, it's not allowed
+      // if it is fiddlestick's item, it's not allowed
+      // if it includes trinket or consumable in tags, it's not allowed
+      // if it has no plaintext, it's probably not allowed
+      // if it is hidden, it's probably not allowed
+      // if its price is less than 1450, it's not allowed UNLESS it's a support item OR it's boots
+      let inStore = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".inStore"
+      )[0];
+      let consumable = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".consumed"
+      )[0];
+      let onSummonersRift = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".maps.11"
+      )[0];
+      let ornn = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".requiredAlly"
+      )[0];
+      let fiddle = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".requiredChampion"
+      )[0];
+      let tag = jsonpath.query(items, "$.data." + randomItem + ".tags")[0];
+      let plaintext = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".plaintext"
+      )[0];
+      let hidden = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".hideFromAll"
+      )[0];
+      let price = jsonpath.query(
+        items,
+        "$.data." + randomItem + ".gold.total"
+      )[0];
+
+      if (inStore !== undefined || inStore === false) {
+        i--;
+        continue;
+      }
+
+      if (consumable !== undefined || consumable === true) {
+        i--;
+        continue;
+      }
+
+      if (onSummonersRift !== true) {
+        i--;
+        continue;
+      }
+
+      if (ornn !== undefined || ornn === "Ornn") {
+        i--;
+        continue;
+      }
+
+      if (fiddle !== undefined || fiddle === "FiddleSticks") {
+        i--;
+        continue;
+      }
+
+      if (plaintext === "") {
+        i--;
+        continue;
+      }
+
+      if (tag.includes("Trinket") || tag.includes("Consumable")) {
+        i--;
+        continue;
+      }
+
+      if (hidden === true) {
+        i--;
+        continue;
+      }
+
+      if (price <= 1450 && !tag.includes("GoldPer") && !tag.includes("Boots")) {
+        i--;
+        continue;
+      }
+
+      let itemName = jsonpath
+        .query(items, "$.data." + randomItem + ".name")
+        .toString();
+
+      itemList.push(randomItem);
+      itemNameList.push(itemName);
+    }
+  });
+}
 
 function checkItems(rawItemsData, item) {}
